b61eb2ea4fac06e4eda7b80b001acf38447c13b0
LCR Peter Simon <lcrpeter7@gmail.com>
2023-08-15 08:21:41 +0100

    Js-Part3-version2

---
+++ JS-Part3/password-maker/index.js
@@ -0,0 +1,36 @@
+const dataLowercase = "azertyuiopqsdfghjklmwxcvbn";
+const dataUppercase = dataLowercase.toUpperCase();
+const dataNumbers = "0123456789";
+const dataSymbols = "*$^ù!:;,?.\"/§µ£¨%&é'(-è_çà)";
+const rangeValue = document.getElementById("password-length");
+const passwordOutput = document.getElementById("password-output");
+function generatePassword() {
+  let data = [];
+  let password = "";
+
+  if (lowercase.checked) data.push(...dataLowercase);
+  if (uppercase.checked) data.push(...dataUppercase);
+  if (numbers.checked) data.push(...dataNumbers);
+  if (symbols.checked) data.push(...dataSymbols);
+
+  if (data.length === 0) {
+    alert("Veuillez sélectionner des critères");
+    return;
+  }
+
+  for (let i = 0; i < rangeValue.value; i++) {
+    password += data[Math.floor(Math.random() * data.length)];
+  }
+  passwordOutput.value = password;
+
+  passwordOutput.select();
+  document.execCommand("copy");
+
+  generateButton.textContent = "copié";
+
+  setTimeout(() => {
+    generateButton.textContent = "Générer mot de passe";
+  }, 2800);
+}
+
+generateButton.addEventListener("click", generatePassword);
